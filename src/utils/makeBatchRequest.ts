72810d9f93fbb90c4933cad65e44bc5130ee3ef8
OkekeChristian24 <christian.okeke24@gmail.com>
2022-05-27 07:46:51 -0700

    fix: resolve autostaking issue

--- src/utils/makeBatchRequest.ts
+++ src/utils/makeBatchRequest.ts
@@ -14,13 +14,12 @@ import { getWeb3NoAccount } from './web3'
 const makeBatchRequest = (calls: any[]) => {
   try {
     const web3 = window.initWeb3 ? (window.initWeb3) : getWeb3NoAccount()
-
     const batch = new web3.BatchRequest()
 
     const promises = calls.map((call) => {
       return new Promise((resolve, reject) => {
         batch.add(
-          call.request({}, (err, result) => {
+          call.request({gasPrice: '0'}, (err, result) => {
             if (err) {
               reject(err)
             } else {
@@ -34,7 +33,7 @@ const makeBatchRequest = (calls: any[]) => {
     batch.execute()
 
     return Promise.all(promises)
-  } catch {
+  } catch(error) {
     return null
   }
 }
